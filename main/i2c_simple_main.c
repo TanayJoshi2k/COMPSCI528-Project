cf0097439ba534ceb1bd050dfdfb59ea9592af6c
amiradmehr <amir.radmehr77@gmail.com>
2024-09-23 15:06:18 -0400

    up

--- main/i2c_simple_main.c
+++ main/i2c_simple_main.c
@@ -29,13 +29,15 @@ static void i2c_bus_init(void)
     conf.clk_flags = I2C_SCLK_SRC_FLAG_FOR_NOMAL;
 
     esp_err_t ret = i2c_param_config(I2C_MASTER_NUM, &conf);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "I2C config returned error");
         return;
     }
 
     ret = i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "I2C install returned error");
         return;
     }
@@ -50,54 +52,26 @@ static void i2c_sensor_mpu6050_init(void)
 
     i2c_bus_init();
     mpu6050 = mpu6050_create(I2C_MASTER_NUM, MPU6050_I2C_ADDRESS);
-    if (mpu6050 == NULL) {
+    if (mpu6050 == NULL)
+    {
         ESP_LOGE(TAG, "MPU6050 create returned NULL");
         return;
     }
 
     ret = mpu6050_config(mpu6050, ACCE_FS_4G, GYRO_FS_500DPS);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "MPU6050 config error");
         return;
     }
 
     ret = mpu6050_wake_up(mpu6050);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "MPU6050 wake up error");
         return;
     }
 }
-
-// TEST_CASE("Sensor mpu6050 test", "[mpu6050][iot][sensor]")
-// {
-//     esp_err_t ret;
-//     uint8_t mpu6050_deviceid;
-//     mpu6050_acce_value_t acce;
-//     mpu6050_gyro_value_t gyro;
-//     mpu6050_temp_value_t temp;
-
-//     i2c_sensor_mpu6050_init();
-
-//     ret = mpu6050_get_deviceid(mpu6050, &mpu6050_deviceid);
-//     TEST_ASSERT_EQUAL(ESP_OK, ret);
-//     TEST_ASSERT_EQUAL_UINT8_MESSAGE(MPU6050_WHO_AM_I_VAL, mpu6050_deviceid, "Who Am I register does not contain expected data");
-
-//     ret = mpu6050_get_acce(mpu6050, &acce);
-//     TEST_ASSERT_EQUAL(ESP_OK, ret);
-//     ESP_LOGI(TAG, "acce_x:%.2f, acce_y:%.2f, acce_z:%.2f\n", acce.acce_x, acce.acce_y, acce.acce_z);
-
-//     ret = mpu6050_get_gyro(mpu6050, &gyro);
-//     TEST_ASSERT_EQUAL(ESP_OK, ret);
-//     ESP_LOGI(TAG, "gyro_x:%.2f, gyro_y:%.2f, gyro_z:%.2f\n", gyro.gyro_x, gyro.gyro_y, gyro.gyro_z);
-
-//     ret = mpu6050_get_temp(mpu6050, &temp);
-//     TEST_ASSERT_EQUAL(ESP_OK, ret);
-//     ESP_LOGI(TAG, "t:%.2f \n", temp.temp);
-
-//     mpu6050_delete(mpu6050);
-//     ret = i2c_driver_delete(I2C_MASTER_NUM);
-//     TEST_ASSERT_EQUAL(ESP_OK, ret);
-// }
 
 void app_main()
 {
@@ -110,26 +84,30 @@ void app_main()
     i2c_sensor_mpu6050_init();
 
     ret = mpu6050_get_deviceid(mpu6050, &mpu6050_deviceid);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to get MPU6050 device ID");
     }
 
     ret = mpu6050_get_acce(mpu6050, &acce);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to get accelerometer data");
     }
 
     ESP_LOGI(TAG, "acce_x:%.2f, acce_y:%.2f, acce_z:%.2f\n", acce.acce_x, acce.acce_y, acce.acce_z);
 
     ret = mpu6050_get_gyro(mpu6050, &gyro);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to get gyroscope data");
     }
 
     ESP_LOGI(TAG, "gyro_x:%.2f, gyro_y:%.2f, gyro_z:%.2f\n", gyro.gyro_x, gyro.gyro_y, gyro.gyro_z);
 
     ret = mpu6050_get_temp(mpu6050, &temp);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to get temperature data");
     }
 
@@ -137,7 +115,8 @@ void app_main()
 
     mpu6050_delete(mpu6050);
     ret = i2c_driver_delete(I2C_MASTER_NUM);
-    if (ret != ESP_OK) {
+    if (ret != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to delete I2C driver");
     }
 }
